cfb025264b5a4828814ee357dd9875d8568d442e
legimia <legimia@gmail.com>
2025-09-28 15:24:21 -0400

    remove

--- src/components/ui/textarea.jsx
+++
@@ -1,2 +0,0 @@
-import React from 'react'
-export function Textarea(props){ return <textarea {...props} className={`w-full rounded-xl border border-neutral-300 px-3 py-2 text-sm focus:outline-none focus:ring-2 focus:ring-emerald-500 ${props.className||''}`} /> }
